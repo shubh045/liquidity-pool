dd82e9fe773f0373d3ad2c15eb8344067d7afe03
Shubham <shubh6441@gmail.com>
2024-02-07 00:23:09 +0530

    change artifacts path to frontend

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,6 +5,9 @@ require('dotenv').config();
 module.exports = {
   solidity: "0.8.20",
   networks: {
+    hardhat: {
+      chainId: 1337
+    },
     sepolia: {
       url: process.env.SEPOLIA_TESTNET_RPC_URL,
       accounts: [process.env.TESTNET_PRIVATE_KEY]
@@ -13,5 +16,8 @@ module.exports = {
       url: process.env.REDBELLY_DEVNET_RPC_URL,
       accounts: [process.env.TESTNET_PRIVATE_KEY]
     }
+  },
+  paths: {
+    artifacts: "./frontend/app/artifacts"
   }
 };
