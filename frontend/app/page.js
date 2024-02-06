33ef19e3cdd0b8086c999926f19394b3a9e452a1
Shubham <shubh6441@gmail.com>
2024-02-07 00:19:14 +0530

    start fronetend part

---
+++ frontend/app/page.js
@@ -0,0 +1,11 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+   <div>
+    <input type="text" />
+    <input type="text" />
+    <button>Swap</button>
+   </div>
+  );
+}
